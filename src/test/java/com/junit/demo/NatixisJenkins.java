62f6db327b8f82a62f4dac31e9b366345248c878
Madhu <madhujagu456@gmail.com>
2023-03-17 12:54:45 +0530

    this all got uploaded

---
+++ src/test/java/com/junit/demo/NatixisJenkins.java
@@ -0,0 +1,13 @@
+package com.junit.demo;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class NatixisJenkins {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
